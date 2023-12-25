409214240ea808e35210a2af3523f74b2934a262
Georgi <gogotin5@gmail.com>
2023-12-25 14:06:47 +0200

    Implemented some basic matrix functions (create, fill,print,delete)

--- Minesweeper/Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper/Minesweeper.cpp
@@ -4,6 +4,44 @@ const int MAX_SIZE = 10;
 const int MIN_SIZE = 3;
 const int MIN_MINES = 1;
 
+void print_matrix(char** matrix,const int rows,const int cols)
+{
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+char** create_matrix(const int rows,const int cols) {
+    char** matrix = new char* [rows];
+
+    for (int i = 0; i < rows; ++i) 
+    {
+        matrix[i] = new char[cols]; 
+    }
+
+    return matrix;
+}
+
+void initialise_matrix(char** matrix, const int rows, const int cols)
+{
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+           matrix[i][j] = 35;
+        }
+    }
+}
+
+void delete_matrix(char** matrix, int rows) {
+    for (int i = 0; i < rows; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+
 int main()
 {
     //Input size of matrix
@@ -26,4 +64,10 @@ int main()
         std::cout << "Please enter a valid amount of mines between " << MIN_MINES << " to " << n << " : ";
         std::cin >> mines;
     }
+
+    char** matrix = create_matrix(n, n);
+    initialise_matrix(matrix, n, n);
+    print_matrix(matrix, n, n);
+    delete_matrix(matrix, n);  // Sending prayers this works.
+
 }
