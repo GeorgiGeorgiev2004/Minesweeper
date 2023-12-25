8ba1b219384e13def18aba63880a3c8f565b0fbc
Georgi <gogotin5@gmail.com>
2023-12-25 16:37:13 +0200

    fixed some minor inconveniences

--- Minesweeper/Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper/Minesweeper.cpp
@@ -3,6 +3,15 @@
 const int MAX_SIZE = 10;
 const int MIN_SIZE = 3;
 const int MIN_MINES = 1;
+// I'm not really sure if using built in randomisers is allowed 
+// Having pondered on this question for a while I came to a realisation
+// I appear to be lacking the capabilities to implement it in another way. As such I will take advantage.
+void place_mines(char** matrix,const int rows,const int cols,const int mine_count) 
+{
+    bool map[MAX_SIZE][MAX_SIZE] = { {0} };
+
+
+}
 
 void print_matrix(char** matrix,const int rows,const int cols)
 {
@@ -27,11 +36,13 @@ char** create_matrix(const int rows,const int cols) {
 
 void initialise_matrix(char** matrix, const int rows, const int cols)
 {
+    int element = 35; // ASCII code for #
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-           matrix[i][j] = 35;
+            matrix[i][j] = element;
         }
     }
+
 }
 
 void delete_matrix(char** matrix, int rows) {
@@ -40,6 +51,7 @@ void delete_matrix(char** matrix, int rows) {
     }
     delete[] matrix;
 }
+
 
 
 int main()
@@ -64,10 +76,9 @@ int main()
         std::cout << "Please enter a valid amount of mines between " << MIN_MINES << " to " << n << " : ";
         std::cin >> mines;
     }
-
     char** matrix = create_matrix(n, n);
     initialise_matrix(matrix, n, n);
     print_matrix(matrix, n, n);
-    delete_matrix(matrix, n);  // Sending prayers this works.
+    delete_matrix(matrix, n);  // Actively hoping this works.
 
 }
